4eb8ba9741e6767ce814648254a11a2303f5794a
julianabot <julianarolluqui@gmail.com>
2023-10-11 15:44:29 +0800

    remove comments index.ts

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,6 @@ export function initClient(
         statusMap = _statusMap;
     }
 }
-
 export async function submitForm(
     formData: FormData,
     statusHandler: FormStatusHandler,
@@ -34,35 +33,25 @@ export async function submitForm(
     }
 
     function startTimeoutMonitor() {
-        // Set up a delayed action using setTimeout
         setTimeout(async () => {
-            // Check if this function has already handled an update
             if (isLastUpdate) {
                 return;
             }
 
-            // Turn off the listener for changes in the form data
             formRef.off('child_changed', onValueChange);
-            // Fetch a snapshot of the current form data
             const snapshot = await formRef.once('value');
             const formData = snapshot.val(); // Extract the data from the snapshot
 
-            // Get the current status of the form
             let newStatus = formData["@status"];
 
-            // Indicate that this is the last update this function will handle
             isLastUpdate = true;
 
-            // Check if the current status is a "terminal state" (e.g., finished, canceled, or an error)
             if (isTerminalState(newStatus)) {
-                // If it's a terminal state, update the status and notify the handler
                 statusHandler(newStatus, {
                     ...formData,
                     "@status": newStatus,
                 }, isLastUpdate);
             } else {
-                // If it's not a terminal state, consider it an error due to a timeout
-                // Set a new error status and notify the handler with an error message
                 newStatus = getStatusValue("error");
                 statusHandler(newStatus, {
                     ...formData,
@@ -74,78 +63,59 @@ export async function submitForm(
         }, timeout || DEFAULT_TIMEOUT);
     }
 
-    // Create a reference to the user ID by extracting it from the '@docPath' property
     const userId = formData["@docPath"].split("/")[1];
 
-    // Create a reference to a location in the Firebase database
-    // The location is determined by the user ID, and '.push()' generates a unique key
     const formRef = db.ref(`forms/${userId}`).push();
 
-    // Set initial data at this location, including '@status' as 'submit' and the form data as JSON
     await formRef.set({
         "@status": getStatusValue("submit"),
         formData: JSON.stringify(formData),
     });
 
-    // Initialize variables to track the current status of the form and whether this function handled the last update
     let currentStatus = getStatusValue("submit");
     let isLastUpdate = false;
 
-    // Set up a listener to detect changes in the form data, specifically in the '@status' property
     const onValueChange = formRef.on('child_changed', snapshot => {
-        // This code will execute whenever the '@status' property changes
-        const changedVal = snapshot.val(); // Get the new status value
-        const changedKey = snapshot.key;   // Get the key that changed
+        const changedVal = snapshot.val();
+        const changedKey = snapshot.key;
 
-        // Check if the key exists and if it's '@status'
         if (!changedKey || changedKey !== "@status") {
-            return; // If not, do nothing and return
+            return;
         }
 
-        // Extract the new status value as a FormStatus
         const newStatus = changedVal as FormStatus;
         console.log("Outside startTimeoutMonitor\nformData: " + formData + "\n@status: " + newStatus + "\nisLastUpdate: " + isLastUpdate + "\nisTerminalState: " + isTerminalState(newStatus))
         // Check if the new status is a "terminal state" (e.g., finished, canceled, or an error)
         if (isTerminalState(newStatus)) {
-            // If it's a terminal state, mark this as the last update and turn off the listener
             isLastUpdate = true;
             formRef.off('child_changed', onValueChange);
         }
 
-        // Notify the status handler about the new status
-        // Also, update the form data with the new status for the handler
         statusHandler(newStatus, {...formData, "@status": newStatus}, isLastUpdate);
 
-        // Update the current status to the new status
         currentStatus = newStatus;
 
     });
 
-    // Start the timeout monitor function to check for changes in the form's status
     startTimeoutMonitor();
 
-    // Return an object with two functions: 'cancel' and 'unsubscribe'
     return {
-        // 'cancel' function allows canceling the form submission under certain conditions
         cancel: async () => {
             const delay = formData["@delay"];
             if (delay) {
-                // Check if there's a delay and if the current status is 'delay'
                 if (currentStatus === getStatusValue("delay")) {
                     console.log("Cancelling form");
-                    // Update the status to 'cancel' to cancel the form submission
                     await formRef.update({"@status": getStatusValue("cancel")});
-                    return true; // Return 'true' to indicate successful cancellation
+                    return true;
                 } else {
                     console.log("Delay has elapsed. Can't cancel the form");
-                    return false; // Return 'false' to indicate cancellation is not possible
+                    return false;
                 }
             } else {
                 console.log("Can only cancel the form with a delay");
-                return false; // Return 'false' to indicate cancellation is not possible
+                return false;
             }
         },
-        // 'unsubscribe' function turns off the listener set up earlier
         unsubscribe: () => {
             formRef.off('child_changed', onValueChange);
         }
