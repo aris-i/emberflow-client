9fef2fd3742b2456401e9d732ef765fcdb874d67
Lloyd Alemania <lloydalemania@gmail.com>
2025-02-24 06:30:14 +0800

    Resolved an issue where callback is not detached properly.

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import {FormData, FormStatus, FormStatusHandler} from "./types";
 import * as admin from "firebase-admin";
 import {database} from "firebase-admin";
 import {Timestamp} from "firebase-admin/firestore";
+import {DataSnapshot} from "@firebase/database-types";
 
 let db: database.Database;
 let _uid: string;
@@ -86,7 +87,7 @@ export const submitCancellableForm = async (
 
     let isLastUpdate = false;
 
-    const onValueChange = formRef.on('child_changed', snapshot => {
+    const onValueChange = (snapshot: DataSnapshot) => {
         const changedVal = snapshot.val();
         const changedKey = snapshot.key;
 
@@ -122,7 +123,9 @@ export const submitCancellableForm = async (
             );
         }
         currentStatus = newStatus;
-    });
+    };
+
+    formRef.on('child_changed', onValueChange);
 
     startTimeoutMonitor();
 
